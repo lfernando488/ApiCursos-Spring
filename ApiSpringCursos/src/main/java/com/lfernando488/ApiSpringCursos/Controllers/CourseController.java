4e8cb74ac77910a6eec50e7da977916024faac63
Luiz Fernando <lfernando488@gmail.com>
2023-02-20 20:17:37 -0300

    Metodo de exclusão controller de curso

--- ApiSpringCursos/src/main/java/com/lfernando488/ApiSpringCursos/Controllers/CourseController.java
+++ ApiSpringCursos/src/main/java/com/lfernando488/ApiSpringCursos/Controllers/CourseController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -62,6 +63,16 @@ public class CourseController {
 					resFound.setCategory(course.getCategory());
 					Course updated = courseRepository.save(resFound);
 					return ResponseEntity.ok().body(updated);
+				})
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable("id") Long id){
+		return courseRepository.findById(id)
+				.map(resFound -> {
+					courseRepository.deleteById(id);
+					return ResponseEntity.noContent().<Void>build();
 				})
 				.orElse(ResponseEntity.notFound().build());
 	}
